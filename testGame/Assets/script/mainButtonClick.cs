fb66f6d4949e46df95e9f0dccfbff1ebb6fccb67
sashay <sashay@sfu.ca>
2020-04-02 23:09:44 -0700

    made cookie clickers

---
+++ testGame/Assets/script/mainButtonClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class mainButtonClick : MonoBehaviour
+{
+   public GameObject textBox;
+
+   public void ClickTheButton(){
+       GlobalCookies.CookieCount += 1;
+   }
+}
